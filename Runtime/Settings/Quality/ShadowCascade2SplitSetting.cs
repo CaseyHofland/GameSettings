9505ce32c6ad15afb54f2d3ecf0a0628dc10b9a2
Casey Hofland <hofland.casey@gmail.com>
2020-11-19 18:49:32 +0100

    Quality Settings starting with S

---
+++ Runtime/Settings/Quality/ShadowCascade2SplitSetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameSettings
+{
+    [CreateAssetMenu(fileName = "ShadowCascade2SplitSetting", menuName = "Game Settings/Quality/Shadow Cascade 2 Split")]
+    public class ShadowCascade2SplitSetting : FloatSetting
+    {
+        public override string settingName => "Shadow Cascade 2 Split";
+
+        public override float min => 0f;
+        public override float max => 1f;
+
+        public override float value
+        {
+            get => QualitySettings.shadowCascade2Split;
+            set => QualitySettings.shadowCascade2Split = Mathf.Clamp(value, min, max);
+        }
+    }
+}
+
